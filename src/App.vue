88bfefe62fcc0d99270ebc17a9d96052a183c049
oleksandrsonich <oleksandr.sonich@wondermentapps.com>
2021-10-01 17:17:59 +0300

    Send progress

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <p>123</p>
-  <TweetsList />
-  <Facebook />
+  <div class="wrapper">
+    <div class="header">Header</div>
+    <div class="wrapper-content">
+      <div class="view">
+        <div class="container">
+          <TweetsList />
+          <Facebook />
+        </div>
+      </div>      
+    </div>
+    <div class="footer"> <p>Footer</p></div>
+  </div>
+ 
+  
 </template>
 
 <script>
